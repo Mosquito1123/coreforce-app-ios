c9517ea79dfbb1bda50e7f503ce41a8c84ef21d6
EDY <zhangwentong1123@icloud.com>
2024-06-28 15:46:53 +0800

    MOD:租期临近提醒

--- hfpower/Modules/More/Views/PersonalDevicesViewCell.swift
+++ hfpower/Modules/More/Views/PersonalDevicesViewCell.swift
@@ -46,6 +46,16 @@ class PersonalDevicesViewCell: PersonalContentViewCell {
         rentalView.translatesAutoresizingMaskIntoConstraints = false
         return rentalView
     }()
+    lazy var batteryExpirationView: PersonalExpirationView = {
+        let view = PersonalExpirationView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    lazy var bikeExpirationView: PersonalExpirationView = {
+        let view = PersonalExpirationView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     // MARK: - Static
     override class func cellIdentifier() -> String {
         return String(describing: self)
@@ -117,6 +127,8 @@ private extension PersonalDevicesViewCell {
 
         self.stackView.addArrangedSubview(self.bikeStackView)
         self.stackView.addArrangedSubview(self.bikeRentalView)
+        self.bikeExpirationView.deviceType = "电车"
+        self.stackView.addArrangedSubview(self.bikeExpirationView)
     }
     
     private func setupLayout() {
@@ -204,5 +216,137 @@ class PersonalRentalView:UIView{
     override func layoutSubviews() {
         super.layoutSubviews()
         self.sureButton.setImagePosition(type: .imageRight, Space: 6)
+    }
+}
+class PersonalExpirationView:UIView{
+    var sureAction:ButtonActionBlock?
+    var deviceType = "电池"{
+        didSet{
+            let fullText = "您的\(deviceType)租期剩余\(remainingDays)，为了不影响您正常使用，请尽快续租。"
+            let attributedString = NSMutableAttributedString(string: fullText,attributes: [.font:UIFont.systemFont(ofSize: 12)])
+
+            // 定义颜色
+            let defaultColor = UIColor(named: "333333") ?? .black
+            let specialColor = UIColor(named: "FF4D4F") ?? .red
+
+            // 设置默认颜色
+            attributedString.addAttribute(.foregroundColor, value: defaultColor, range: NSRange(location: 0, length: fullText.count))
+
+            // 设置特殊文本的颜色
+            let nsRange = (fullText as NSString).range(of: remainingDays)
+            attributedString.addAttribute(.foregroundColor, value: specialColor, range: nsRange)
+
+            // 使用 attributedString，例如设置到一个 UILabel 上
+            titleLabel.attributedText = attributedString
+        }
+    }
+    var remainingDays = "1天17小时"{
+        didSet{
+            let fullText = "您的\(deviceType)租期剩余\(remainingDays)，为了不影响您正常使用，请尽快续租。"
+            let attributedString = NSMutableAttributedString(string: fullText,attributes: [.font:UIFont.systemFont(ofSize: 12)])
+
+            // 定义颜色
+            let defaultColor = UIColor(named: "333333") ?? .black
+            let specialColor = UIColor(named: "FF4D4F") ?? .red
+
+            // 设置默认颜色
+            attributedString.addAttribute(.foregroundColor, value: defaultColor, range: NSRange(location: 0, length: fullText.count))
+
+            // 设置特殊文本的颜色
+            let nsRange = (fullText as NSString).range(of: remainingDays)
+            attributedString.addAttribute(.foregroundColor, value: specialColor, range: nsRange)
+
+            // 使用 attributedString，例如设置到一个 UILabel 上
+            titleLabel.attributedText = attributedString
+        }
+    }
+    private(set) lazy var titleLabel: UILabel = {
+        // 创建文本
+        let remainingDays = "1天17小时"
+        let fullText = "您的电池租期剩余\(remainingDays)，为了不影响您正常使用，请尽快续租。"
+        let attributedString = NSMutableAttributedString(string: fullText,attributes: [.font:UIFont.systemFont(ofSize: 12)])
+
+        // 定义颜色
+        let defaultColor = UIColor(named: "333333") ?? .black
+        let specialColor = UIColor(named: "FF4D4F") ?? .red
+
+        // 设置默认颜色
+        attributedString.addAttribute(.foregroundColor, value: defaultColor, range: NSRange(location: 0, length: fullText.count))
+
+        // 设置特殊文本的颜色
+        let nsRange = (fullText as NSString).range(of: remainingDays)
+        attributedString.addAttribute(.foregroundColor, value: specialColor, range: nsRange)
+
+        // 使用 attributedString，例如设置到一个 UILabel 上
+        let label = UILabel()
+        label.attributedText = attributedString
+
+        // 显示 Label
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    lazy var sureButton:UIButton = {
+        let button = UIButton(type: .custom)
+        button.frame = CGRect(x: 0, y: 0, width: 82, height: 31)
+        button.tintAdjustmentMode = .automatic
+        let layerView = UIView()
+        layerView.frame = CGRect(x: 0, y: 0, width: 82, height: 31)
+
+        let bgLayer1 = CAGradientLayer()
+        bgLayer1.colors = [UIColor(red: 0.98, green: 0.91, blue: 0.76, alpha: 1).cgColor, UIColor(red: 0.99, green: 0.79, blue: 0.45, alpha: 1).cgColor]
+        bgLayer1.locations = [0, 1]
+        bgLayer1.frame = layerView.bounds
+        bgLayer1.startPoint = CGPoint(x: 0.96, y: 0.5)
+        bgLayer1.endPoint = CGPoint(x: 0.5, y: 0.5)
+        layerView.layer.addSublayer(bgLayer1)
+        button.setBackgroundImage(layerView.asImage(), for: .normal)
+        button.setBackgroundImage(layerView.asImage(), for: .highlighted)
+        button.setImage(UIImage(named: "ic_arrow_right_brown"), for: .normal)
+        button.setImage(UIImage(named: "ic_arrow_right_brown"), for: .highlighted)
+        button.setTitleColor(UIColor(named: "6C4627"), for: .normal)
+        button.setTitleColor(UIColor(named: "6C4627"), for: .highlighted)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        button.setTitle("立即续租", for: .normal)
+        button.setTitle("立即续租", for: .highlighted)
+        button.layer.cornerRadius = 15.5
+        button.layer.masksToBounds = true
+        button.addTarget(self, action: #selector(sureButtonTapped(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubviews()
+        setupLayout()
+        
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    private func setupSubviews() {
+        self.backgroundColor = UIColor(named: "FFEBE9")
+        self.layer.cornerRadius = 10
+        self.addSubview(titleLabel)
+        self.addSubview(sureButton)
+    }
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            titleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12),
+            titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: self.sureButton.leadingAnchor, constant: -12),
+            self.sureButton.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            self.sureButton.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -10),
+            self.sureButton.widthAnchor.constraint(equalToConstant: 82),
+            self.sureButton.heightAnchor.constraint(equalToConstant: 31),
+        ])
+    }
+    @objc func sureButtonTapped(_ sender:UIButton){
+        self.sureAction?(sender)
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.sureButton.setImagePosition(type: .imageRight, Space: 10)
     }
 }
